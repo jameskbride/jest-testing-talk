18bb995ce29add0484f553644a1b6b5010c3e87a
Jim Kirkbride <jim.kirkbride@beam.dental>
2019-03-11 15:05:51 -0400

    Adding shallow matcher examples.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Hello from './Hello'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      learnAbout: 'React'
+    }
+  }
+
+  updateLearnAbout = () => {
+    this.setState({learnAbout: 'Jest'})
+  }
+
   render() {
+    let learnAbout = this.state.learnAbout
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Hello id="hello-wrapper" />
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn {learnAbout}
           </a>
         </header>
+
+        <button id='change-button' onClick={this.updateLearnAbout}>Change Learning Experience</button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,35 @@ import React from 'react'
 import {shallow} from 'enzyme'
 
 import App from './App'
+import Hello from './Hello'
 
-it('renders without crashing', () => {
-  let wrapper = shallow(<App/>)
+describe('App', () => {
 
-  expect(wrapper.text()).toContain('Learn React')
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  it('renders without crashing', () => {
+    expect(wrapper.text()).toContain('Learn React')
+  })
+
+  it('has an app link', () => {
+    expect(wrapper.find('.App-link').first()).toHaveLength(1)
+  })
+
+  it('has a Hello component', () => {
+    expect(wrapper.find(Hello).first()).toHaveLength(1)
+  })
+
+  it('has a Hello component with an id', () => {
+    expect(wrapper.find('#hello-wrapper').first()).toHaveLength(1)
+  })
+
+  it('updates the message when the Change Learning Experience button is clicked', () => {
+    wrapper.find('#change-button').first().simulate('click')
+
+    expect(wrapper.text()).toContain('Learn Jest')
+  })
 })
